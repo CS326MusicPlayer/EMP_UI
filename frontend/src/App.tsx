37d28df05715da172e3032413c30818a657269cd
Cobblestone5517 <kjy11gold@gmail.com>
2025-04-04 01:45:19 -0400

    Initial stab for changing background color (depending on the time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import { ConfigProvider } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PiStatus from './components/PiStatus';
 import TimeWeather from './components/TimeWeather';
 import MusicPlayer from './components/MusicPlayer';
 import './App.css';
 
 function App(): React.ReactElement {
+  // Hardcoded for demonstration, replace with actual MQTT data
+  // #b3e6ff for day, #3a3a5c for night
+  const [backgroundColor, setBackgroundColor] = useState('#b3e6ff');
+  
+  const mqttMessage = { bgColor: '#b3e6ff' };
+  const changeBackgroundColor = (newColor: string) => {
+    document.documentElement.style.setProperty('--background-color', newColor);
+  };
+
+  useEffect(() => {
+    // Simulate receiving a message from MQTT
+    setBackgroundColor(mqttMessage.bgColor);
+    changeBackgroundColor(backgroundColor);
+  }, [mqttMessage]);
 
   return (
     <ConfigProvider

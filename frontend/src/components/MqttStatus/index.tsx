b26b8b5a390a4a876a7688c2a7012e53f20af7c8
Cobblestone5517 <kjy11gold@gmail.com>
2025-04-13 00:25:43 -0400

    Actually, connection button can be simpler

--- frontend/src/components/MqttStatus/index.tsx
+++ frontend/src/components/MqttStatus/index.tsx
@@ -27,6 +27,7 @@ export default function MqttStatus({
 ): React.ReactElement {
   const { selectedPiId, setSelectedPiId, piList } = usePiSelection();
   const [timeDisplay, setTimeDisplay] = useState<string>('');
+  const [showStatus, setShowStatus] = useState<boolean>(true);
 
   // Update the time display initially and when lastConnectedTime changes
   useEffect(() => {
@@ -36,6 +37,7 @@ export default function MqttStatus({
   // Handle mouse enter to refresh the time display
   const handleMouseEnter = () => {
     setTimeDisplay(formatTimeAgo(lastConnectedTime));
+    setShowStatus(false);
   };
 
 
@@ -49,22 +51,27 @@ export default function MqttStatus({
 
   const onlineContent = (
     <div>
-      <p style={{ color: '#616161' }}>Your device seems to be online!</p>
-      <p style={{ color: '#8e8e8e' }}>Connected to MQTT Server</p>
+      <h3 style={{ color: '#616161' }}>Your device seems to be online!</h3>
+      <p style={{ color: 'var(--black)' }}>Click to disconnect from the MQTT server</p>
+      <p style={{ color: 'var(--emerald2)' }}>Connected to MQTT Server</p>
     </div>
   );
 
   const offlineContent = (
     <div>
       <p style={{ color: '#616161' }}>Your device seems to be offline</p>
-      <p style={{ color: '#8e8e8e' }}>Last seen: {timeDisplay}</p>
+      <p style={{ color: 'var(--black)' }}>Click to connect to the MQTT server</p>
+      <p style={{ color: 'var(--gray)' }}>Last seen: {timeDisplay}</p>
     </div>
   );
 
   return (
     <div className={classes.container}>
       <span className={classes.status}>
-        <Popover content={mqttConnected ? onlineContent : offlineContent} trigger="hover" placement="top">
+        <Popover
+          content={mqttConnected ? onlineContent : offlineContent}
+          trigger="hover"
+          placement="top">
           <div
             className={classes.statusIconContainer}
             style={{
@@ -72,69 +79,56 @@ export default function MqttStatus({
               borderColor: `${mqttConnected ? '#d1f3e7' : '#f3f3f3'}`
             }}
             onMouseEnter={handleMouseEnter}
+            onMouseLeave={() => setShowStatus(true)}
+            onClick={mqttConnected ? onDisconnect : onConnect}
           >
-            {mqttConnected ? <LuCheck className={classes.statusIcon} /> : <LuEllipsis className={classes.statusIcon} />}
+            {mqttConnected ?
+              (showStatus ? <LuPower className={classes.statusIcon} /> : <LuCheck className={classes.statusIcon} />):
+              (showStatus ? <LuPowerOff className={classes.statusIcon} /> : <LuEllipsis className={classes.statusIcon} />)
+            }
           </div>
         </Popover>
         <p className={classes.statusText}>{mqttConnected ? 'Online' : 'Offline'}</p>
-        {/* Pi Swap button */}
-        { mqttConnected &&
-          <>
-            <Popover
-              content={
-                piList.length > 1 ?
-                  <div>
-                    <h3 style={{ color: 'var(--black)' }}>Switch Pi</h3>
-                    <p style={{ color: 'var(--black)' }}>Click to switch to another Pi</p>
-                    <p style={{ color: 'var(--gray)' }}>Current Pi: {selectedPiId}</p>
-                    <p style={{ color: 'var(--gray)' }}>Available Pis: {piList.join(', ')}</p>
-                  </div>
-                  :
-                  <div>
-                    <h3 style={{ color: 'var(--black)' }}>Switch Pi</h3>
-                    <p style={{ color: 'var(--black)' }}>Only one Pi available</p>
-                  </div>
-              }
-              trigger="hover"
-              placement="top"
-            >
-              <div
-                className={piList.length > 1 && !musicIsFading ? classes.piSwitchButton : classes.piSwitchButtonDisabled}
-                onClick={() => {
-                  // Only rotate if there are multiple Pis
-                  if (piList.length > 1 && !musicIsFading) {
-                    const nextPiId = getNextPiId(selectedPiId, piList);
-                    setSelectedPiId(nextPiId);
-                  }
-                }}
-              >
-                <SwapOutlined style={{ color: 'var(--black)' }} />
-              </div>
-            </Popover>
-            <p className={classes.statusText}>Pi: {selectedPiId}</p>
-          </>
-        }
       </span>
       {musicIsFading && (<Spin className={classes.musicStatusSpinner} />)}
-      <span className={classes.connection}>
-        <Popover
-          content={
-            <div>
-              <h3 style={{ color: 'var(--black)' }}>{mqttConnected ? 'Disconnect' : 'Connect'}</h3>
-              <p style={{ color: 'var(--black)' }}>
-                {mqttConnected ? 'Click to disconnect from the MQTT server' : 'Click to connect to the MQTT server'}
-              </p>
-            </div>
-          }
-          trigger="hover"
-          placement="top"
-        >
-          {mqttConnected ? (
-            <button className={classes.connButton} onClick={onDisconnect}><LuPowerOff /></button>
-          ) : (
-            <button className={classes.connButton} onClick={onConnect}><LuPower /></button>
-          )}
-        </Popover>
+      {/* Pi Swap button */}
+      <span className={classes.piStatusContainer}>
+        { mqttConnected &&
+            <>
+              <p className={classes.statusText}>Pi: {selectedPiId}</p>
+              <Popover
+                content={
+                  piList.length > 1 ?
+                    <div>
+                      <h3 style={{ color: 'var(--black)' }}>Switch Pi</h3>
+                      <p style={{ color: 'var(--black)' }}>Click to switch to another Pi</p>
+                      <p style={{ color: 'var(--gray)' }}>Current Pi: {selectedPiId}</p>
+                      <p style={{ color: 'var(--gray)' }}>Available Pis: {piList.join(', ')}</p>
+                    </div>
+                    :
+                    <div>
+                      <h3 style={{ color: 'var(--black)' }}>Switch Pi</h3>
+                      <p style={{ color: 'var(--black)' }}>Only one Pi available</p>
+                    </div>
+                }
+                trigger="hover"
+                placement="top"
+              >
+                <div
+                  className={piList.length > 1 && !musicIsFading ? classes.piSwitchButton : classes.piSwitchButtonDisabled}
+                  onClick={() => {
+                    // Only rotate if there are multiple Pis
+                    if (piList.length > 1 && !musicIsFading) {
+                      const nextPiId = getNextPiId(selectedPiId, piList);
+                      setSelectedPiId(nextPiId);
+                    }
+                  }}
+                >
+                  <SwapOutlined style={{ color: 'var(--black)' }} />
+                </div>
+              </Popover>
+            </>
+        }
       </span>
     </div>
   );

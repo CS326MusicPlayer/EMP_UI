5b15d541eacd92b4d8dea132e247f29a95099fc9
Cobblestone5517 <kjy11gold@gmail.com>
2025-04-14 23:49:26 -0400

    Initial stab for custom broker modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { ConfigProvider, FloatButton } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
-import React from 'react';
+import { GithubOutlined, SettingOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
 import { SensorPreferencesProvider } from './contexts/SensorPreferencesContext';
 import { PiSelectionProvider } from './contexts/PiSelectionContext';
 import AppContent from './AppContent';
 import './App.css';
 
 function App(): React.ReactElement {
+  const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
+
   return (
     <ConfigProvider
       theme={{
@@ -19,12 +21,23 @@ function App(): React.ReactElement {
       <SensorPreferencesProvider>
         <PiSelectionProvider>
           <AppContent />
-          <FloatButton
-            icon={<GithubOutlined />}
-            href="https://github.com/CS326MusicPlayer/EMP_UI" 
-            target="_blank"
-            tooltip="View project on GitHub"
-          />
+          <FloatButton.Group
+            shape="circle"
+            trigger='hover'
+            icon={<InfoCircleOutlined />}
+          >
+            <FloatButton
+              icon={<GithubOutlined />}
+              href="https://github.com/CS326MusicPlayer/EMP_UI" 
+              target="_blank"
+              tooltip="View project on GitHub"
+            />
+            <FloatButton
+              icon={<SettingOutlined className="floatSettingsButton" />}
+              tooltip="Settings"
+              onClick={() => setIsSettingModalOpen(true)}
+            />
+          </FloatButton.Group>
         </PiSelectionProvider>
       </SensorPreferencesProvider>
     </ConfigProvider>
